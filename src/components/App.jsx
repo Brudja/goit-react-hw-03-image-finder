49e2be48b1299bf31445b73b4662578d8a55c448
Brudja <n0669378587@gmail.com>
2022-10-27 09:47:15 +0300

    попытка 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import { Component } from "react";
 import { Searchbar } from "./Searchbar/Searchbar"
 import { ImageGallery } from "./ImageGallery/ImageGallery"
 import { Button } from "./Button/Button"
-import { Modal } from "./Modal/Modal"
 import {Status} from "../services/config"
 import css from "./App.module.css"
 
